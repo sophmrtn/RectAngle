7c02869fad972bd502438e167b5b51859f272ae1
Iani <iani.gayo.20@ucl.ac.uk>
2021-06-24 22:26:15 +0100

    Changed batch size to 8; cuda to 2

--- classifier_train_weightedloss.py
+++ classifier_train_weightedloss.py
@@ -1,6 +1,8 @@
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
 import torch 
 from torchvision import transforms, models
 from torchvision.transforms import Normalize
@@ -155,7 +157,7 @@ if cuda_available:
 train_data = h5py.File('/raid/candi/Iani/MRes_project/dataset/dataset_zip/train.h5', 'r')
 #train_data = h5py.File('/Users/iani/Documents/Reg2Seg/dataset/train.h5')
 train_dataset = ClassifyDataLoader(train_data)
-train_DL = torch.utils.data.DataLoader(train_dataset, batch_size = 16, shuffle = True)
+train_DL = torch.utils.data.DataLoader(train_dataset, batch_size = 8, shuffle = True)
 
 val_data = h5py.File('/raid/candi/Iani/MRes_project/dataset/dataset_zip/val.h5', 'r')
 #val_data = h5py.File('/Users/iani/Documents/Reg2Seg/dataset/val.h5')
