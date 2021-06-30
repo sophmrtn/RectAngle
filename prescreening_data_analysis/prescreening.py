7fb1a1fb2c847490dbdfc95f60539cef64b020d6
Imraj-Singh <imraj.singh.20@ucl.ac.uk>
2021-06-30 10:52:24 +0100

    prescreening extra stuff

--- prescreening_data_analysis/prescreening.py
+++ prescreening_data_analysis/prescreening.py
@@ -10,10 +10,13 @@ import numpy as np
 # tp_gt_pred_pixels = pixelwise #pixels true positive
 
 random_data = loadmat('random.mat')
-vote_data = loadmat('vote.mat')
+vote_data  = loadmat('vote.mat')
 mean_data = loadmat('mean.mat')
-combine_50_data = loadmat('combine_50.mat')
-combine_75_data = loadmat('combine_75.mat')
+combine_50 = loadmat('combine_50.mat')
+combine_75 = loadmat('combine_75.mat')
+
+#random_data = loadmat('mean.mat')
+
 
 positive_frames = random_data['gt_frame'].astype(bool)[0]
 negative_frames = np.abs(random_data['gt_frame']-1).astype(bool)[0]
@@ -29,44 +32,63 @@ def get_boolean_prescreened(dict_label,threshold):
     mask = np.copy(dict_label['screening_prob_frame'])
     mask[mask<threshold] = 0.
     mask[mask>=threshold] = 1.
-    preds = dict_label['pred_pixels'][0]
+    preds = np.copy(dict_label['pred_pixels'][0])
     preds[preds>0] = 1
     return (mask[0]*preds).astype(bool)
+
 
 #def get_diff_thresh_dice
 
 ## Dice on all frames for different thresholds
-random_data = get_dice(random_data)
+#random_data = get_dice(random_data)
 mask = get_boolean_prescreened(random_data,1)
-plt.figure(figsize=(3, 3))
+plt.figure(figsize=(6, 6))
 data = np.array([])
 false_negatives = np.array([])
-thresholds = np.linspace(0,20,100)
-for i in thresholds:
-    mask = get_boolean_prescreened(random_data,i)
-    # On all frames
-    #data = np.append(data,np.mean((random_data['dice'][0]*mask.astype(float))))
-    # Only on screened frames
-    data = np.append(data,np.mean((random_data['dice'][0]*mask.astype(float))[mask]))
-    # false negatives
-    false_negatives = np.append(false_negatives,positive_frames[~mask].sum()/positive_frames.sum())
-print(false_negatives)
-plt.plot(thresholds,data)
-plt.plot(thresholds,false_negatives)
+thresholds = np.linspace(0,5,100)
+leg = np.array(['random_data', 'vote_data', 'mean_data','combine_50','combine_75'])
+for ii, data1 in enumerate([random_data, vote_data, mean_data,combine_50,combine_75]):
+    data1 = get_dice(data1)
+    data = np.array([])
+    false_negatives = np.array([])
+    for i in thresholds:
+        mask = get_boolean_prescreened(data1,i)
+        subset = np.copy(mask)
+        for i in range(len(subset)):
+            if positive_frames[i] == False & mask[i] == False:
+                subset[i] = False
+            else:
+                subset[i] = True
+        # On screened frams including false negatives
+        data = np.append(data,np.mean((data1['dice'][0]*mask.astype(float))[mask]))
+        # Only frames that get passed classifier
+        #data = np.append(data,np.mean((random_data['dice'][0]*mask.astype(float))[mask]))
+        # false negatives
+        false_negatives = np.append(false_negatives,positive_frames[~mask].sum()/positive_frames.sum())
+    plt.plot(thresholds,data, label = leg[ii])
+#plt.plot(thresholds,false_negatives)
+plt.ylabel("Dice score")
+plt.xlabel("Threshold values")
+plt.legend() 
 plt.tight_layout()
 
 ## Histogram of false positive pixels on negative frames
 random_data = get_dice(random_data)
 # SET THRESHOLD
-thresh = 0.5
+thresh = 5
 mask = get_boolean_prescreened(random_data,thresh)
-plt.figure(figsize=(3, 3))
+plt.figure(figsize=(6, 6))
 # Non-pre-screen
-data = random_data['pred_pixels'][0][negative_frames]
-plt.hist(data, label = "no-screen", bins = 10)
+preds = np.copy(random_data['pred_pixels'][0])
+preds[preds>0] = 1
+data = random_data['pred_pixels'][0][negative_frames*preds.astype(bool)]
+
+plt.hist(data, label = "no-screen")
 # Get pre-screened
-data = (random_data['pred_pixels'][0]*mask.astype(float))[negative_frames]
-plt.hist(data, label = "screen", bins = 10)
+for i in np.linspace(0,5,11):
+    mask = get_boolean_prescreened(random_data,i)
+    data = (random_data['pred_pixels'][0]*mask.astype(float))[negative_frames*mask]
+    plt.hist(data, label = "screen, thresh {}".format(i))
 plt.xlabel("Number of FP pixels per negative segmented frame")
 plt.ylabel("Number of negative frames")
 plt.legend() 
