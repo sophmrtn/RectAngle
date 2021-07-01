5fd7fb721e3f22e151333b0484182bf11ff6eeb4
giuliominore <60554590+giuliominore@users.noreply.github.com>
2021-07-01 13:42:23 +0100

    Add files via upload

--- prescreening_data_analysis/prescreening.py
+++ prescreening_data_analysis/prescreening.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import confusion_matrix
 import pandas as pd
-import sys
+import seaborn as sns
 
 # Fields of the loaded .mat files
 # gt_frame = framewise binary ground truth
@@ -11,6 +11,9 @@ import sys
 # gt_pixels = pixelwise #pixels ground truth
 # pred_pixels = pixelwise #pixels predicted
 # tp_gt_pred_pixels = pixelwise #pixels true positive
+
+#plt.fill_between(np.mean()+np.std(), np.mean()-std(), alpha=0.3)
+sns.set_palette(palette='Set2')
 
 random_data = loadmat('random.mat')
 vote_data = loadmat('vote.mat')
@@ -29,11 +32,11 @@ def get_FP_prescreened(dict_label, threshold):
 
 
 index = np.linspace(0, 5, 6)
-columns = ['random', 'vote', 'mean', 'combine_25', 'combine_50', 'combine_75']
+columns = ['Vote', 'Random', 'Mean', 'Combine (25)', 'Combine (50)', 'Combine (75)']
 df_FP = pd.DataFrame(index=index, columns=columns)
 df_FN = df_FP.copy()
 
-for ii, data1 in enumerate([random_data, vote_data, mean_data, combine_25, combine_50, combine_75]):
+for ii, data1 in enumerate([vote_data, random_data, mean_data, combine_25, combine_50, combine_75]):
     for i in index:
         tn, fp, fn, tp = get_FP_prescreened(data1, i)
         df_FP.iloc[np.int(i)][ii] = fp / (fp + tn)
@@ -45,28 +48,16 @@ print(df_FN)
 fig, axes = plt.subplots(2, 1, figsize=(6,6))
 
 df_FP.plot.bar(rot=0, ax=axes[0], legend=False)
-axes[0].set_xlabel("Threshold values")
-axes[0].set_ylabel("FP rate")
-axes[0].legend(bbox_to_anchor= (0.08, 1.01), ncol=3, title='Label sampling')
+#axes[0].set_xlabel("Threshold Values")
+axes[0].set_ylabel("FPR")
+axes[0].legend(bbox_to_anchor= (0.08, 1.01), ncol=3, title='Method')
 df_FN.plot.bar(rot=0, ax=axes[1], legend=False)
-axes[1].set_xlabel("Threshold values")
-axes[1].set_ylabel("FN rate")
+axes[1].set_xlabel("Threshold")
+axes[1].set_ylabel("FNR")
 #handles, labels = axes[0].get_legend_handles_labels()
 #fig.legend(handles, labels, loc="upper center", ncol=3, title='Label sampling')
 fig.tight_layout()
 
-df_FP.plot.bar(rot=0)
-plt.xlabel("Threshold values")
-plt.ylabel("FP rate")
-plt.legend(title="Label sampling", bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.tight_layout()
-df_FN.plot.bar(rot=0)
-plt.xlabel("Threshold values")
-plt.ylabel("FN rate")
-plt.legend(title="Label sampling", bbox_to_anchor=(1.05, 1), loc='upper left')
-plt.tight_layout()
-
-# random_data = loadmat('mean.mat')
 
 positive_frames = random_data['gt_frame'].astype(bool)[0]
 negative_frames = np.abs(random_data['gt_frame'] - 1).astype(bool)[0]
@@ -97,9 +88,9 @@ plt.figure(figsize=(6, 6))
 data = np.array([])
 false_negatives = np.array([])
 thresholds = np.linspace(0, 5, 100)
-leg = np.array(['random', 'vote', 'mean', 'combine_25', 'combine_50', 'combine_75'])
+leg = np.array(['Vote', 'Random', 'Mean', 'Combine (25)', 'Combine (50)', 'Combine (75)'])
 
-for ii, data1 in enumerate([random_data, vote_data, mean_data, combine_25, combine_50, combine_75]):
+for ii, data1 in enumerate([vote_data, random_data, mean_data, combine_25, combine_50, combine_75]):
     data1 = get_dice(data1)
     data = np.array([])
     false_negatives = np.array([])
@@ -119,9 +110,9 @@ for ii, data1 in enumerate([random_data, vote_data, mean_data, combine_25, combi
         false_negatives = np.append(false_negatives, positive_frames[~mask].sum() / positive_frames.sum())
     plt.plot(thresholds, data, label=leg[ii])
 # plt.plot(thresholds,false_negatives)
-plt.ylabel("Dice score")
-plt.xlabel("Threshold values")
-plt.legend(title="Label sampling")
+plt.ylabel("Mean Dice")
+plt.xlabel("Threshold")
+plt.legend(title="Method")
 plt.tight_layout()
 
 ## Histogram of false positive pixels on negative frames
@@ -134,9 +125,11 @@ plt.figure(figsize=(6, 6))
 preds = np.copy(combine_25['pred_pixels'][0])
 preds[preds > 0] = 1
 data = combine_25['pred_pixels'][0][negative_frames * preds.astype(bool)]
+pixel_area = 0.177994000000000*0.161290000000000  # mm^2
+data *= pixel_area
 
-bin_ranges = np.linspace(0, 62356, 11)  # 64189 is the maximal FP pixel count for combine_25
-plt.hist(data, bins=bin_ranges, edgecolor='black', label="no screen")
+bin_ranges = np.linspace(0, 1791, 11)  # 1791 is the maximal FP pixel count for combine_25
+plt.hist(data, bins=bin_ranges, color="tab:blue", edgecolor='black', label="none")
 
 # Get pre-screened
 color_list = ['tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink']
@@ -144,9 +137,10 @@ color_list = ['tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', '
 for i in np.linspace(0, 5, 6):
     mask = get_boolean_prescreened(combine_25, i)
     data = (combine_25['pred_pixels'][0] * mask.astype(float))[negative_frames * mask]
+    data *= pixel_area
     plt.hist(data, bins=bin_ranges, color=color_list[np.int(i)], edgecolor='black',
-             label="thresh {}".format(i))  # range=[0, sys.float_info.max]
-plt.xlabel("Number of FP pixels per negative segmented frame")
+             label="{}".format(i))
+plt.xlabel("Area of FP pixels per negative segmented frame ($mm^2$)")
 plt.ylabel("Number of negative frames")
-plt.legend(title="Screening strategy")
+plt.legend(title="Threshold")
 plt.show()
